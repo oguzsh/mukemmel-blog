ce0b83ffe1c5422b009c6e79ff42601003ac5cb9
Oguzhan <oguzhan824@gmail.com>
2019-12-08 02:55:56 +0300

    heroku linki ile değiştirildi

--- pages/[postId].js
+++ pages/[postId].js
@@ -26,7 +26,9 @@ const BlogPost = ({ post }) => (
 );
 
 BlogPost.getInitialProps = async ({ req, query }) => {
-  const res = await fetch(`http://localhost:3000/api/post/${query.postId}`);
+  const res = await fetch(
+    `https://oguzshblog.herokuapp.com/api/post/${query.postId}`
+  );
   const json = await res.json();
   return { post: json.post };
 };
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Home = ({ posts }) => (
 );
 
 Home.getInitialProps = async ({ req }) => {
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("https://oguzshblog.herokuapp.com/api/posts");
   const json = await res.json();
   return { posts: json.posts };
 };
