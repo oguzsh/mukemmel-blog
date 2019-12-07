1cc84d65e521b3cca3314149695016669589b455
Oguzhan <oguzhan824@gmail.com>
2019-12-08 01:45:03 +0300

    Tüm stilleri toplamak için dosya oluşturuldu

---
+++ components/styles.js
@@ -0,0 +1,74 @@
+const styles = {
+
+    // Header
+    heroStyle: {
+        marginTop: "30px",
+        textAlign: "center"
+    },
+    titleStyle: {
+        fontSize: "40px",
+        fontWeight: 600,
+        textTransform: "uppercase",
+        textDecoration: "none",
+        color: "black"
+    },
+    subtitleStyle: {
+        color: "grey",
+        textTransform: "uppercase",
+        margin: "0"
+    },
+
+    //Social
+
+
+    // Posts
+    postContainer: {
+        marginTop: "40px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        alignItems: "center",
+        width: "100%"
+    },
+    postCard: {
+        position: "relative",
+        width: "250px",
+        height: "300px",
+        margin: "10px",
+        padding: "10px",
+        backgroundColor: "#263238",
+        borderRadius: 10,
+        overflow: "hidden",
+        textDecoration: "none"
+    },
+    postTitle: {
+        marginTop: "40px",
+        fontSize: "20px",
+        textAlign: "left",
+        color: "white"
+    },
+    postText: {
+        color: "white",
+        fontSize: "15px"
+    },
+    postDate: {
+        position: "absolute",
+        textAlign: "center",
+        fontWeight: 600,
+        color: "white",
+        fontSize: "12px",
+        borderRadius: 10,
+        top: "10px",
+        padding: "5px",
+        backgroundColor: "#651fff"
+    },
+    links: {
+        color: "#651fff",
+        textDecoration: "none"
+    },
+
+
+};
+
+export default styles;
